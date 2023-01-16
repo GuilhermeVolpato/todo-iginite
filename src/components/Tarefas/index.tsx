750e6f6b1699fd180c6b0aed5ad2dbce0dc303e5
Guilherme Chaves Volpato <volpato2103@gmail.com>
2023-01-15 22:52:06 -0300

    feito a lista de tarefa e terminado o front-end da aplicacao

--- src/components/Tarefas/index.tsx
+++ src/components/Tarefas/index.tsx
@@ -1,25 +1,27 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
+import {useState} from "react";
+
 
 type Props = {
   name: string;
   onRemove: () => void;
+  onPress: () => void;
 };
 
-export function Tarefas({ name, onRemove }: Props) {
+export function Tarefas({ name, onRemove, onPress }: Props) {
+  const[concluido, setConcluido] = useState()
+
   return (
     <View style={styles.container}>
-      <Text style={styles.name}>{name}</Text>
-
-      <TouchableOpacity
-        style={styles.buttom}
-        activeOpacity={0.7}
-        onPress={onRemove}
-      >
-        <Text style={styles.buttomText}>
-          -
-        </Text>
+      <TouchableOpacity >
+        <View style={styles.circulo}></View>
       </TouchableOpacity>
+      <Text style={styles.name}>{name}</Text>
+      <Image
+      source={require("../../images/trash.png")}
+      style={{marginRight: 10, height: 35, width:35}}
+      />
     </View>
   )
 }
